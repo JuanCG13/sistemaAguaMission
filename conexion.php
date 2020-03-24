938767b8608248af8cc6f624f3b4974fee8da96f
JuanCG13 <58788407+JuanCG13@users.noreply.github.com>
2020-03-24 19:01:25 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -4,15 +4,39 @@
     $s = 'localhost';
     $u = 'root';
     $p = '';
-    $bd = 'aguabd';
+    
 
-    $conexion = new mysqli($s,$u,$p,$bd);
+    $conexion = mysqli_connect($s,$u,$p);
+
 
     if ($conexion->connect_errno)
     {
         echo "no conectado"; 
     }
-    
+
+    $db_selected = mysqli_select_db($conexion,'aguabd');
+
+    if (!$db_selected) {
+        // esto uso para saber si existe o no la base de datos
+        $sql = "CREATE DATABASE aguabd"; 
+
+        if(mysqli_query($conexion,$sql))
+        {
+            echo 'se creo la bd';
+
+        }
+        mysqli_close($conexion);
+
+
+        include('agregartb.php');
+
+        
+   }
+   else 
+   {
+       $bd = 'aguabd';
+   }
+   
 
     
    
