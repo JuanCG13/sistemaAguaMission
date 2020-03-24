40b959325101213affcf802014ad42673b37f771
JuanCG13 <58788407+JuanCG13@users.noreply.github.com>
2020-03-24 09:02:16 -0400

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,27 @@
+function confirmaratras()
+{
+	if(confirm('Desea salir de este Registro?'))
+		return true;
+	else
+		return false;
+}
+
+function confirmarguardado()
+{
+	if(confirm('Desea Guardar este registro?'))
+		return true;
+	else
+		return false;
+}
+
+function confirmarborrar()
+{
+	if(confirm('Desea borrar este registro?'))
+		return true;
+	else
+		return false;
+}
+
+
+
+
